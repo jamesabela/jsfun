0a7267c5b5e92a62c64ab339a480248d3d08cd36
jamesabela <abela.james@gmail.com>
2026-05-31 18:00:23 +0800

    feat: add beginner tutorial for variable manipulation with interactive exercise and solution script

---
+++ beginners/variables.py
@@ -0,0 +1,20 @@
+#Input
+# ""
+
+#output
+# Bond James Bond
+
+#End
+# Intro to Python: Variables
+
+# Learn more: https://jamesabela.github.io/jsfun/beginners/variables.html
+
+firstName = "James"
+lastName = "Bond"
+# This is a variable, it can change as the program works
+
+print(firstName)
+print(lastName)
+
+# So can you do the right intro with Bond James Bond?
+print(firstName,lastName)
