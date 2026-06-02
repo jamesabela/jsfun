6fff1642e81f4c02f28726ac7b61bd5161f7a6f0
jamesabela <abela.james@gmail.com>
2026-06-02 19:55:47 +0800

    feat: add prime number generation example with execution time benchmarking

---
+++ examples/primes.py
@@ -0,0 +1,22 @@
+import datetime
+def generate_n_primes(N):
+    primes  = [2]
+    chkthis = 3
+    while len(primes) < N:
+        ptest = False
+        for i in primes:
+            if chkthis%i == 0:
+                ptest = True
+                break
+        primes  += [] if ptest else [chkthis]
+        chkthis += 2
+    return primes
+
+# Tests time on your PC
+a = datetime.datetime.now()
+print(generate_n_primes(1000))
+b = datetime.datetime.now()
+c = b-a
+seconds = c.total_seconds()
+
+print("it took",seconds)
